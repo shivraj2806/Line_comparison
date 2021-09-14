848c5e83e01b37fceeb045398dabc5791fc0ea62
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-14 20:53:04 +0530

    Added: Check line equality

--- src/com/Bridgelabz/Line_Comparison.java
+++ src/com/Bridgelabz/Line_Comparison.java
@@ -1,14 +1,43 @@
 package com.Bridgelabz;
 
+import java.util.Scanner;
+
 public class Line_Comparison {
+    public double calculateLength() {
+        int x1, x2, y1, y2;
 
-    static double Coordinates (int x1,int y1,int x2,int y2){
-        double Length=Math.sqrt(Math.pow( x2 - x1, 2 )+ Math.pow( y2 - y1, 2 ));
-        System.out.println("length of a line is " +Length);
-        return (Length);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter x1 point");
+        x1 = input.nextInt();
+        System.out.println("Enter x2 point");
+        x2 = input.nextInt();
+        System.out.println("Enter y1 point");
+        y1 = input.nextInt();
+        System.out.println("Enter y2 point");
+        y2 = input.nextInt();
+
+        double length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of the line between points " + "(" + x1 + "," + y1 + "),(" + x2 + "," + y2 + ") = " + length);
+        return length;
     }
-    public static void main(String[] args) {
-        Coordinates(4,5,6,5);
+    public void equalityOfLines() {
 
+        double Length0f_line1;
+        double Length0f_line2;
+        System.out.println("Length of line 1 is");
+        Length0f_line1 = calculateLength();
+        System.out.println("Length of line 2 is");
+        Length0f_line2 = calculateLength();
+
+        if (Length0f_line1 == Length0f_line2) {
+            System.out.println("Length are equal ");
+        } else{
+            System.out.println("Length are not equal");
+        }
     }
 }
+
+
+
+
---
+++ src/com/Bridgelabz/Main.java
@@ -0,0 +1,8 @@
+package com.Bridgelabz;
+
+public class Main {
+    public static void main(String[] args) {
+        Line_Comparison equality_length = new Line_Comparison();
+        equality_length.equalityOfLines();
+    }
+}
