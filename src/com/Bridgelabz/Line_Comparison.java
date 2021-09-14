eacaa7b01585d05b1d45d353bdaff4e66fa0b2f5
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-14 20:09:35 +0530

    Added: Claculating the length by using cartesian co-ordinate

--- src/com/Bridgelabz/Line_Comparison.java
+++ src/com/Bridgelabz/Line_Comparison.java
@@ -1,7 +1,14 @@
 package com.Bridgelabz;
 
 public class Line_Comparison {
+
+    static double Coordinates (int x1,int y1,int x2,int y2){
+        double Length=Math.sqrt(Math.pow( x2 - x1, 2 )+ Math.pow( y2 - y1, 2 ));
+        System.out.println("length of a line is " +Length);
+        return (Length);
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to line comparison computation program");
+        Coordinates(4,5,6,5);
+
     }
 }
