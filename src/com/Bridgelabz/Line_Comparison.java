3e5630c2d9e22e9bffc19fb4c9cad41ac6457a6f
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-14 19:46:48 +0530

    Welcome message

---
+++ src/com/Bridgelabz/Line_Comparison.java
@@ -0,0 +1,7 @@
+package com.Bridgelabz;
+
+public class Line_Comparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison computation program");
+    }
+}
